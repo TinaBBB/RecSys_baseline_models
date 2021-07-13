2253086457c534308254adedae75d1c179b19dd0
TinaBBB <tinashen.tianshu@gmail.com>
2021-07-13 05:37:57 -0400

    Update run_yelp.sh

--- cluster_bash/yelp_toronto/run_yelp.sh
+++ cluster_bash/yelp_toronto/run_yelp.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 autorec.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 bpr-part1.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 bpr-part2.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 bpr-part3.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 bpr-part4.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 bpr-part5.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 cdae-part1.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 cdae-part2.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 cdae-part3.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 cdae-part4.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner autorec.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner bpr-part1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner bpr-part2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner bpr-part3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner bpr-part4.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner bpr-part5.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner cdae-part1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner cdae-part2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner cdae-part3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner cdae-part4.sh
 
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 nceplrec.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 plrec.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 puresvd.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 vae-part1.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 vae-part2.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 vae-part3.sh
-sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus=4 --gres=gpu:1 vae-part4.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner nceplrec.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner plrec.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner puresvd.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner vae-part1.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner vae-part2.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner vae-part3.sh
+sbatch --nodes=1 --time=96:00:00 --mem=32G --cpus-per-task=4 --gres=gpu:1 --account=rrg-ssanner vae-part4.sh
