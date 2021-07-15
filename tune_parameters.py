16a3a5527993afea7fe04b30ce926b7eef0e398b
TinaBBB <tinashen.tianshu@gmail.com>
2021-07-15 17:18:47 -0400

    update

--- tune_parameters.py
+++ tune_parameters.py
@@ -10,7 +10,8 @@ def main(args):
     params['models'] = {params['models']: models[params['models']]}
     R_train = load_numpy(path=args.path, name=args.train)
     R_valid = load_numpy(path=args.path, name=args.valid)
-    hyper_parameter_tuning(R_train, R_valid, params, save_path=args.name, measure=params['similarity'], gpu_on=args.gpu)
+    R_test = load_numpy(path=args.path, name=args.test)
+    hyper_parameter_tuning(R_train+R_valid, R_test, params, save_path=args.name, measure=params['similarity'], gpu_on=args.gpu)
 
 if __name__ == "__main__":
     # Commandline arguments
@@ -19,6 +20,7 @@ if __name__ == "__main__":
     parser.add_argument('-d', dest='path', default="datax/")
     parser.add_argument('-t', dest='train', default='Rtrain.npz')
     parser.add_argument('-v', dest='valid', default='Rvalid.npz')
+    parser.add_argument('-test', dest='test', default='Rtest.npz')
     parser.add_argument('-y', dest='grid', default='config/default.yml')
     parser.add_argument('-gpu', dest='gpu', action='store_true')
     args = parser.parse_args()
