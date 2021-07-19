2b3351f01eae9113da9b05cff56ae449ebc904ed
TinaBBB <tinashen.tianshu@gmail.com>
2021-07-19 16:49:13 -0400

    update produce results script

--- cluster_bash/yelp_orig/produce_results.sh
+++ cluster_bash/yelp_orig/produce_results.sh
@@ -4,4 +4,4 @@ source ~/nceplrec/bin/activate
 
 cd /home/tinashen/projects/def-ssanner/tinashen/NCE_Projected_LRec_baseline
 
-python reproduce_paper_results.py
+python reproduce_paper_results.py -n yelp_orig_final_result -d datax/yelp_orig/ -p yelp_orig 
