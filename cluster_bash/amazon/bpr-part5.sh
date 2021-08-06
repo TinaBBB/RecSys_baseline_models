40e4d8ab35f9a75bea6a8db21254226a0a9cf7a4
TinaBBB <tinashen.tianshu@gmail.com>
2021-08-05 23:41:43 -0400

    add in amazon cluster bash scripts

---
+++ cluster_bash/amazon/bpr-part5.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+module load python/3.6
+source ~/nceplrec/bin/activate
+
+cd /home/tinashen/projects/def-ssanner/tinashen/NCE_Projected_LRec_baseline
+
+python tune_parameters.py -d datax/amazon/ -n amazon/bpr-part5.csv -y config/bpr-part5.yml
---
+++ cluster_bash/amazon/vae-part1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+module load python/3.6
+source ~/nceplrec/bin/activate
+
+cd /home/tinashen/projects/def-ssanner/tinashen/NCE_Projected_LRec_baseline
+
+python tune_parameters.py -d datax/amazon/ -n amazon/vae-part1.csv -y config/vae-part1.yml
